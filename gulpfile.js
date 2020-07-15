ede6ae2e51543967248d89044aa14a258d0d70e2
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-07-15 09:54:56 +0900

    eslintの設定

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function(done) {
+  // ESLintを実行
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
   // Nodeのソース
   gulp.src("es6/**/*.js")
     .pipe(babel())
