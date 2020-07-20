45e92c5d6fc38b72c2ad0ec30e363518419a11a6
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-07-20 10:46:36 +0900

    ex-06-03-2

---
+++ example/ch06/ex-06-03-2/main.js
@@ -0,0 +1,21 @@
+function f(o) {
+  console.log(`fの中（2つの代入の前）：o.message="${o.message}"`);
+  o.message = "fで最初にセットされた";
+  console.log(`fの中（2つの代入の間）：o.message="${o.message}"`);
+  o = {
+    message: "新しいオブジェクト！",
+  };
+  console.log(`fの中（2つの代入の後）：o.message="${o.message}"`);
+}
+
+let o = {
+  message: "初期値",
+};
+console.log(`fの呼び出し前：o.message="${o.message}"`);
+f(o);
+console.log(`fの呼び出し後：o.message="${o.message}"`);
+
+function e(x) {
+  return `f内のxの値: ${x}`;
+}
+console.log(e()); // "f内のxの値: undefined"
