59beafbefffbbf714970f685f15296a496d45035
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-07-28 10:52:36 +0900

    ex-09-06

---
+++ example/ch09/ex-09-06/main.js
@@ -0,0 +1,42 @@
+// 継承
+class Vehicle { /* 乗り物 */
+  constructor() {
+    this.passengers = []; /* 乗客 */
+    console.log("Vehicleが生成された");
+  }
+  addPassenger(p) { /* 乗客を追加 */
+    this.passengers.push(p);
+  }
+}
+
+class Car extends Vehicle { /* 車は乗り物のサブクラス */
+  constructor() {
+    super(); /* スーパークラスのコンストラクタを呼び出す */
+    console.log("Carが生成された");
+  }
+  deployAirbags() { /* エアバッグを作動させる */
+    console.log("バーンッ！");
+  }
+}
+const v = new Vehicle(); // Vehicleが生成された
+v.addPassenger("太郎");
+v.addPassenger("花子");
+console.log(v.passengers); // [ '太郎', '花子' ]
+
+const c = new Car(); // Vehicleが生成された\nCarが生成された （\nは改行を表す）
+c.addPassenger("景子");
+c.addPassenger("ミドリ");
+console.log(c.passengers); // [ '景子', 'ミドリ' ]
+// v.deployAirbags(); /* これを実行するとエラーになってしまう */
+c.deployAirbags(); // バーンッ！
+
+// ポリモーフィズム
+class Motorcycle extends Vehicle {} /* オートバイは乗り物のサブクラス */
+
+const c2 = new Car(); // Vehicleが生成された\nCarが生成された
+const m = new Motorcycle(); // Vehicleが生成された
+console.log(c instanceof Car); // true
+console.log(c instanceof Vehicle); // true
+console.log(m instanceof Car); // false
+console.log(m instanceof Motorcycle); // true
+console.log(m instanceof Vehicle); // true
