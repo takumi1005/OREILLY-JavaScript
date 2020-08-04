d63453b08cce05d68cab354963061675aa041a7a
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-08-04 12:59:05 +0900

    ex-12-02

---
+++ example/ch12/ex-12-02/main.js
@@ -0,0 +1,21 @@
+// 無限の値を供給するイテレータ
+class FibonacciSequence {
+  [Symbol.iterator]() {
+    let a = 0, b = 1;
+    return {
+      next() {
+        let rval = { value: b, done: false };
+        b += a;
+        a = rval.value;
+        return rval;
+      }
+    };
+  }
+}
+
+const fib = new FibonacciSequence();
+let i = 0;
+for(let n of fib) {
+  console.log(`${i+1}: ${n}`);
+  if(++i > 99) break;
+}
