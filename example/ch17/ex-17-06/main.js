3572a87deb13189e0d936db91e0e99c6bcf4a4d1
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-08-13 12:51:28 +0900

    ex-17-06

---
+++ example/ch17/ex-17-06/main.js
@@ -0,0 +1,17 @@
+// ホワイトスペース
+const tShirts =
+  'Small:      9\n' +
+  'Medium:     5\n' +
+  'Large:      2\n';
+const numbers = tShirts.match(/:\s*[0-9]/g);
+console.log(numbers); // [ ':      9', ':     5', ':      2' ]
+
+const phoneNumber1 = "（0269）99-9876";
+const phoneNumber2 = "0269ー99ー9875";
+console.log(phoneNumber1.replace(/\D/g, '')); // 0269999876
+console.log(phoneNumber2.replace(/\D/g, '')); // 0269999875
+
+const field1 = '   X   ';
+const field2 = '　\t   '; /* 先頭に全角のスペースがある */
+console.log(/\S/.test(field1)); // true
+console.log(/\S/.test(field2)); // false （全角のスペースもホワイトスペース）
