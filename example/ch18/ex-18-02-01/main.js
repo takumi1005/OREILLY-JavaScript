37f231eb6ed4c61ddf6307ad7f35216faaa6cd17
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-08-14 18:09:38 +0900

    ex-18-02-01

---
+++ example/ch18/ex-18-02-01/main.js
@@ -0,0 +1,10 @@
+// DOM要素の操作
+const para1 = document.getElementsByTagName("p")[0];
+console.log(para1.textContent); // これは単純なHTMLファイルです。
+console.log(para1.innerHTML); // これは<b>単純な</b>HTMLファイルです。
+alert("変更します");
+para1.textContent = "HTMLを修正しました。";
+console.log(para1.textContent); // HTMLを修正しました。
+alert("変更します");
+para1.innerHTML = "innerHTMLを使った<b>修正後</b>のHTML";
+console.log(para1.innerHTML); // innerHTMLを使った<b>修正後</b>のHTML
