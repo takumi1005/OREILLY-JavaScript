1cf1ac17087b926e085d24927f2d21e74bed2ba1
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-08-25 18:40:12 +0900

    ex-21-04-03

---
+++ example/ch21/ex-21-04-03/main.js
@@ -0,0 +1,21 @@
+function evaluate(x, c) {
+  return c.a + c.b * x + c.c * Math.pow(x, 2);
+}
+const coefficients = {
+  a: 1,
+  c: 3,
+};
+
+const betterCoefficients = new Proxy(coefficients, {
+  get(target, key) {
+    return target[key] || 0;
+  },
+});
+
+console.log(betterCoefficients.a); // 1
+console.log(betterCoefficients.b); // 0
+console.log(betterCoefficients.c); // 3
+console.log(betterCoefficients.d); // 0
+console.log(betterCoefficients.z); // 0
+console.log(betterCoefficients.anything); // 0
+console.log(evaluate(5, betterCoefficients)); // 76
