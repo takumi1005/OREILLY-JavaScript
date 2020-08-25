74d60fd096ff9bc921c8dee7b8f89390ba3c47c6
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-08-25 15:51:22 +0900

    ex-21-02-03

---
+++ example/ch21/ex-21-02-03/main.js
@@ -0,0 +1,12 @@
+// 配列にsum,avgを追加する例
+const arr = [3, 1.5, 9, 2, 5.2];
+arr.sum = function () {
+  return this.reduce((a, x) => a + x);
+};
+arr.avg = function () {
+  return this.sum() / this.length;
+};
+Object.defineProperty(arr, "sum", { enumerable: false });
+Object.defineProperty(arr, "avg", { enumerable: false });
+console.log(arr.sum()); // 20.7
+console.log(arr.avg()); // 4.14
